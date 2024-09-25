bfbf868af10aa31aea58adbe6893814b85fc9edc
CODSTIL <murthariom@gmail.com>
2024-09-25 16:14:32 +0530

    ui complete

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,36 @@
-
-import express from 'express'
-import {createServer} from 'http'
-import {Server} from 'socket.io'
-import dotenv from 'dotenv'
-import authRoutes from '../backend/routes/authRoutes.js'
-import messageRoutes from '../backend/routes/messageRoutes.js'
-import userRoutes from '../backend/routes/userRoutes.js'
-import { connectDb } from './database/db.js'
-import cookieParser from 'cookie-parser';
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import dotenv from "dotenv";
+import authRoutes from "../backend/routes/authRoutes.js";
+import messageRoutes from "../backend/routes/messageRoutes.js";
+import userRoutes from "../backend/routes/userRoutes.js";
+import { connectDb } from "./database/db.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
+app.use(cors());
 app.use(express.json()); // to parse the incoming request with JSON  paylods (from req.body)
 app.use(cookieParser());
-const server = createServer(app); 
-const io = new Server(server);
 
+const server = createServer(app);
+const io = new Server(server);
 
 const PORT = process.env.PORT || 5000;
 console.log(PORT);
 
-app.use("/api/auth",authRoutes);
-app.use("/api/messages",messageRoutes)
-app.use("/api/users",userRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello</h1>');
+app.get("/", (req, res) => {
+  res.send("<h1>Hello</h1>");
 });
 
-
-
-io.on('connection', (socket) => {
-  console.log('a user connected');
+io.on("connection", (socket) => {
+  console.log("a user connected");
 });
 
 server.listen(PORT, () => {
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,26 @@
+/* eslint-disable no-unused-vars */
 
-import './App.css'
+import "./App.css";
+//import { useAuthContext } from "./context/AuthContext";
+// import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
+ import Home from "./pages/home/Home";
+ import Login from "./pages/Login/Login";
+ import Signup from "./pages/signup/Signup";
+ import {Toaster} from 'react-hot-toast';
 
 function App() {
- 
+ // const { authUser } = useAuthContext();
+
+
+
   return (
-    <>
-      App
-    </>
-  )
+     <div className='p-4 h-screen flex items-center justify-center'>
+         {/* <Login/> */}
+         {/* <Signup/> */}
+         <Home/>
+         <Toaster position="top-center"/>
+     </div>
+  );
 }
 
-export default App
+export default App;
